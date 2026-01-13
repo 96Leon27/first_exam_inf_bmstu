f79e6cba1df0054fba89fa3314cc1f09609acad7
96Leon27 <115868501+96Leon27@users.noreply.github.com>
2026-01-13 15:17:26 +0300

    Update filling_tables.sql

--- filling_tables.sql
+++ filling_tables.sql
@@ -1,11 +1,9 @@
-/* must be */
 INSERT INTO users (name, email, role) VALUES 
     ('Админ', 'admin@shop.ru', 'admin'),
     ('Менеджер', 'manager@shop.ru', 'manager'),
     ('Покупатель', 'customer@shop.ru', 'customer') ON CONFLICT (email) DO NOTHING;
     );
 
-/* additional */
 INSERT INTO products(name, price, stock_quantity) VALUES 
 	('Сок Добрый Мультифрукт 1л', 139, 100),
 	('Дартс', 300, 10);
